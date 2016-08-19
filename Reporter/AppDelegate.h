d816994fda5f417e58c26071305687f304f39dbe
Robin You (liayou) <liayou@cisco.com>
2016-08-19 14:21:18 +0800

    Initial Commit

---
+++ Reporter/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Reporter
+//
+//  Created by Robin on 19/8/2016.
+//  Copyright © 2016 cisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Reporter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reporter
+//
+//  Created by Robin on 19/8/2016.
+//  Copyright © 2016 cisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
